cdecabb6049d55230cda559f85626ffbc46c8215
makstravm <maks7-2-94@gmail.com>
2021-02-26 03:28:20 +0200

    added header with adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,17 @@
-$(function(){
+$(function () {
+  $('.menu__btn, .menu__link').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  })
 
 
+});
 
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+
+  if (scroll >= 100) {
+    $(".menu__wrapper").addClass("dark-header");
+  } else {
+    $(".menu__wrapper").removeClass("dark-header");
+  }
 });
