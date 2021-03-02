736e791f604eb6d814c8bd30024beb7193e1ccd8
makstravm <maks7-2-94@gmail.com>
2021-03-02 02:55:17 +0200

    added section solutions, addaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,44 @@
-$(function () {
+
+
   $('.menu__btn, .menu__link').on('click', function () {
     $('.menu__list').toggleClass('menu__list--active');
   })
 
 
-});
+
+
+const animItems = document.querySelectorAll(`._anim-items`);
+if (animItems.length > 0) {
+  window.addEventListener(`scroll`, animOnScroll);
+
+  function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffSet = offset(animItem).top;
+      const animStart = 4;
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+      if ((pageYOffset > animItemOffSet - animItemPoint) && pageYOffset < (animItemOffSet + animItemHeight)) {
+        animItem.classList.add(`_active`);
+      } 
+    }
+  }
+
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+
+}
+
 
 $(window).scroll(function () {
+
   var scroll = $(window).scrollTop();
 
   if (scroll >= 100) {
@@ -15,3 +47,5 @@ $(window).scroll(function () {
     $(".menu__wrapper").removeClass("dark-header");
   }
 });
+
+
